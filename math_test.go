1fac9cbd2a3ca60e16f5edca050739297488ab49
Kamron <wieeil84@gmail.com>
2023-03-23 10:34:56 +0500

    simple table driven test

--- math_test.go
+++ math_test.go
@@ -2,12 +2,21 @@ package math
 
 import "testing"
 
+type addTest struct {
+	arg1, arg2, expected int
+}
+
+var addTests = []addTest{
+	{2, 3, 5},
+	{4, 8, 12},
+	{6, 9, 15},
+	{3, 10, 13},
+}
+
 func TestAdd(t *testing.T) {
-	got := Add(4, 6)
-	want := 10
-
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
+	for _, test := range addTests {
+		if output := Add(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
 	}
-
 }
